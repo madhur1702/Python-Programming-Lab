82d537e330592d59553297358035b6b12a9d5dde
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2025-03-09 13:49:17 +0530

    Delete Area_of_Triangle.py

--- Area_of_Triangle.py
+++
@@ -1,7 +0,0 @@
-a = float(input("Enter the side of a : "))
-b = float(input("Enter the side of b : "))
-c = float(input("Enter the side of c : "))
-s = (a+b+c)/2
-area = (s*(s-a)*(s-b)*(s-c))**0.5
-print('Area of Triangle is %0.2f' %area)
-
